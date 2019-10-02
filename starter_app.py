7290ed399508b96f9a1a9fb026c2954ea2d7db6d
Ji Woong <park.jiwoong@wustl.edu>
2019-10-02 16:31:30 -0400

    Updating

--- starter_app.py
+++ starter_app.py
@@ -180,8 +180,9 @@ def set_cities_value(available_options):
     Input('drop-down5', "value")])
 
 def update_table(value1, value2, value3, value4, value5):
-    #with open('DecisionTree_Pain_model.pkl', 'rb') as infile:
-    #    loaded_model = pickle.load(infile) # key for loading 
+    filename = 'DecisionTree_Pain_model.pkl' # or .sav
+    with open(filename, 'wb') as outfile:
+        joblib.dump(classifier_DM, outfile) # key for saving
 
     # random_data = [[50, 70,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,
     #      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  1,  0,  0,
